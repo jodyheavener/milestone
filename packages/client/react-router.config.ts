7d8e18ac23fc5a320a39c7a39ff33921a5161ae9
Jody Heavener <jody.heavener@agilebits.com>
2025-10-31 16:42:28 -0600

    Remove vercel for now

--- packages/client/react-router.config.ts
+++ packages/client/react-router.config.ts
@@ -1,9 +1,7 @@
-import { vercelPreset } from "@vercel/react-router/vite";
 import type { Config } from "@react-router/dev/config";
 
 export default {
 	ssr: true,
-	presets: [vercelPreset()],
 	buildDirectory: "dist",
 	future: {
 		v8_middleware: true,
